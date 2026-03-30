98c4042c3d52f01b74043629e785d7ab588102a0
suleiman-MBS <149168290+suleiman-MBS@users.noreply.github.com>
2026-03-30 08:38:30 +0300

    Add files via upload

--- 6-1-back-end-fundamentals/src/App.jsx
+++ 6-1-back-end-fundamentals/src/App.jsx
@@ -358,17 +358,18 @@ import './index.css';
 export default function App() {
   // TODO 9:
   // Create state to store student data
-  // Syntax hint:
-  // const [student, setStudent] = useState(____);
+  const [student, setStudent] = useState(null);
 
   // TODO 8:
   // Request student data from the server when the page loads
-  // Syntax hint:
-  // fetch('http://localhost:3000/student')
-  //   .then((res) => res.json())
-  //   .then((data) => {
-  //     setStudent(data);
-  //   });
+  useEffect(() => {
+    fetch('http://localhost:3000/student')
+      .then((res) => res.json())
+      .then((data) => {
+        setStudent(data);
+      })
+      .catch((err) => console.error("Error fetching data:", err));
+  }, []); // The empty array [] ensures this only runs once when the page loads!
 
   return (
     <main className="app-shell">
